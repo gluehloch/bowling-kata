9aaa31b6d2b456a353cfbe550fccee51dca96b5f
Andre Winkler <gluehloch@googlemail.com>
2010-11-22 17:21:59 +0100

    Init of the repository.

---
+++ src/main/java/de/bowling/Bowler.java
@@ -0,0 +1,15 @@
+package de.bowling;
+
+public class Bowler {
+
+    public Bowler(String _name) {
+        name = _name;
+    }
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+}
